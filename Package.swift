2656f008d9c69dc8a93b7923da8ad6b433bdd6a6
Niklas Holloh <niklas.holloh@adesso.de>
2026-09-09 11:05:56 +0200

    Remove watchOS support temporarily

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ import PackageDescription
 
 let package = Package(
     name: "Networking",
-    platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9), .visionOS(.v1)],
+    platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .visionOS(.v1)],
     products: [
         .library(
             name: "Networking",
